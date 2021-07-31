016851f7aff19b2a5cd68bac6575eee9ad726300
Ricardo Signes <rjbs@semiotic.systems>
2021-07-31 19:48:37 -0400

    Wink::Bank::Debug: a file I had lying around...

---
+++ lib/Wink/Bank/Debug.pm
@@ -0,0 +1,21 @@
+package Wink::Bank::Debug;
+
+use v5.30.0;
+use Moose;
+
+extends 'Wink::Bank';
+
+use experimental qw(signatures);
+
+sub dispatch ($self, $name, $method, @args) {
+  warn sprintf "%6s: %s [ %s ]\n", $name, $method, "@args";
+  return;
+}
+
+sub msleep ($self, $ms) {
+  warn sprintf "%6s: %sms\n", 'sleep', $ms;
+  return;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
