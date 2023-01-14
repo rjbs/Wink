607c2f52e4d50fd745a4369cf548e8559a1a0b9a
Ricardo Signes <rjbs@semiotic.systems>
2023-01-14 14:24:09 -0500

    get_serial_dev_map: idiotic code to get hidraw dev by s/n

--- lib/Wink/Util.pm
+++ lib/Wink/Util.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use experimental qw(signatures);
 
-use Sub::Exporter -setup => [ qw(to_rgb) ];
+use Sub::Exporter -setup => [ qw(to_rgb get_serial_dev_map) ];
 
 sub to_rgb ($arg) {
   $arg =~ s/^#//;
@@ -15,6 +15,44 @@ sub to_rgb ($arg) {
   Carp::confess("bad rgb") unless $arg =~ /\A[0-9a-f]{6}/ia;
 
   return unpack 'C*', pack 'H*', $arg;
+}
+
+# pass me an arrayref of /dev/... devices on the usb bus
+sub get_serial_dev_map ($devices) {
+  my %device;
+  DEV: for my $blink_dev (@$devices) {
+    my @info = `udevadm info $blink_dev`;
+    my ($path)   = map {; /^P: (\S+)$/m ? $1 : () } @info;
+    my ($serial) = map {; /ID_SERIAL_SHORT=([[:xdigit:]]+)/ ? $1 : () } @info;
+
+    unless ($path and $serial) {
+      warn "Couldn't figure out $blink_dev\n";
+      next DEV;
+    }
+
+    $device{$serial} = {
+      path => $path,
+    };
+  }
+
+  HID: for my $hid_dev (</dev/hidraw*>) {
+    next unless -w $hid_dev;
+
+    my ($path) = map {; /^P: (\S+)$/m ? $1 : () } `udevadm info $hid_dev`;
+    unless ($path) {
+      warn "No path line for $hid_dev!\n";
+      next HID;
+    }
+
+    my @found = grep {; index($path, $device{$_}{path}) == 0 } keys %device;
+
+    if (@found > 1) { warn "too many matches for $hid_dev\n"; next HID; }
+    if (@found < 1) { warn "no many matches for $hid_dev\n";  next HID; }
+
+    $device{$found[0]}{hid} = $hid_dev;
+  }
+
+  return { map { $_ => $device{$_}{hid} } keys %device };
 }
 
 1;
